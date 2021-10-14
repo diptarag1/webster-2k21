cab126f97ed63ddb76675bf5db740117ded46e6a
DuP-491 <divyansh.20198117@mnnit.ac.in>
2021-10-14 09:43:54 +0530

    signin signout

--- user/views.py
+++ user/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate,logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
 from django.contrib.auth.models import User
+from  django.http import HttpResponse
 # Create your views here.
 
 def signup(request):
@@ -18,6 +19,28 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'user/signup.html', {'form': form})
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        cuser = User.objects.filter(username=username)
+        if len(cuser) == 1:
+            if not cuser[0].is_active:
+                return HttpResponse("hi")
+            elif user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return HttpResponse("hi")
+        else:
+            return HttpResponse("hi")
+    return render(request, 'user/signin.html')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
 
 def profile(request,uname):
     context={}
