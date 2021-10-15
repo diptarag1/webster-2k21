e68daa861cc42e0cde56b51394841239a358900a
DuP-491 <divyansh.20198117@mnnit.ac.in>
2021-10-15 16:20:58 +0530

    star

--- Repos/models.py
+++ Repos/models.py
@@ -7,16 +7,19 @@ rw_dir = '~/git_test_repos'
 
 # Create your models here.
 class Repo(models.Model):
+    parent=models.ForeignKey("self",null=True,on_delete=models.CASCADE)
     name=models.CharField(max_length=30)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
     collaborators = models.ManyToManyField(User, related_name='collaborators',blank=True)
     repoURL = models.CharField(max_length=30) #ownerName/repoName
+    star = models.ManyToManyField(User,related_name='star',blank=True)
 
     def __str__(self):
         return self.repoURL 
 
     def save(self, *args, **kwargs):
         gitRepo.init(os.path.join(rw_dir, self.repoURL))
+        self.repoURL=str(self.owner) + '/' + self.name
         super().save(*args, **kwargs)
 
     
--- Repos/views.py
+++ Repos/views.py
@@ -1,46 +1,75 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, render, redirect,HttpResponse
 from .forms import RepoCreateForm, AddCollaboratorForm
 from .models import Repo
 from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+
+
 
 # Create your views here.
 def init_Repo(request):
     if request.method == 'POST':
         form = RepoCreateForm(request.POST)
         if form.is_valid():
-            new_repo = Repo(owner = request.user, repoURL = form.cleaned_data['rname'],name=form.cleaned_data['rname'])
-            new_repo.repoURL = str(new_repo.owner) + '/' + new_repo.repoURL
+            new_repo = Repo(owner=request.user, repoURL=form.cleaned_data['rname'], name=form.cleaned_data['rname'])
+            new_repo.repoURL = str(new_repo.owner) + '/' + new_repo.name
             new_repo.save()
-            return redirect('home') #for now
+            return redirect('home')  # for now
     else:
         form = RepoCreateForm()
     return render(request, 'Repos/repoCreate.html', {'form': form})
 
-def detail_repo(request,name,owner):
-    context={}
-    repo = Repo.objects.filter(name=name).filter(owner__username=owner).first()
-    context['repo']=repo
-    return render(request,'Repos/repo_detail.html',context=context)
 
-def delete_repo(request,name,owner):
-    context={}
+def detail_repo(request, name, owner):
+    context = {}
+    repo = Repo.objects.filter(name=name).filter(owner__username=owner).first()
+    context['repo'] = repo
+    return render(request, 'Repos/repo_detail.html', context=context)
+
+
+def delete_repo(request, name, owner):
+    context = {}
     repo = Repo.objects.filter(name=name).filter(owner__username=owner).first()
     repo.delete()
     return redirect('home')
 
+
 def add_remove_collaborator(request, ownerUsername, repoName):
     rName = str(ownerUsername) + '/' + repoName
-    curRepo = get_object_or_404(Repo, repoURL = rName)
+    curRepo = get_object_or_404(Repo, repoURL=rName)
     if request.method == 'POST':
         form = AddCollaboratorForm(request.POST)
         if form.is_valid():
-            collaborator = User.objects.filter(username = form.cleaned_data['collaboratorUsername']).first()
-            if(curRepo.collaborators.filter(username = collaborator.username).exists()):
+            collaborator = User.objects.filter(username=form.cleaned_data['collaboratorUsername']).first()
+            if (curRepo.collaborators.filter(username=collaborator.username).exists()):
                 curRepo.collaborators.remove(collaborator)
             else:
                 curRepo.collaborators.add(collaborator)
             curRepo.save()
-            return redirect('home') #for now
+            return redirect('home')  # for now
     else:
         form = AddCollaboratorForm()
     return render(request, 'Repos/addCollaborator.html', {'form': form})
+
+
+def star(request):
+    id = request.POST.get('id')
+    repo = Repo.objects.get(id=id)
+    if request.user in repo.star.all():
+        repo.star.remove(request.user)
+    else:
+        repo.star.add(request.user)
+    context = {
+        'repo': repo,
+    }
+    html = render_to_string('Repos/star-section.html', context, request=request)
+    return JsonResponse({'html': html})
+
+def forkg(request,id):
+    print(id)
+    # parent=Repo.objects.get(id=id)
+    # print(id)
+    # new_repo=Repo.objects.create(parent=parent,owner=request.user,name=parent.name)
+    # new_repo.save()
+    return HttpResponse("hi")
