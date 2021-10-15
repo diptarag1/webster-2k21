ef386d8e7405f16547c3db905623b3facef2b5b7
DuP-491 <divyansh.20198117@mnnit.ac.in>
2021-10-15 18:03:33 +0530

    issue create+issue list

--- Repos/forms.py
+++ Repos/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import Repo
+from .models import Repo,Issue
 
 class RepoCreateForm(forms.Form):
     rname = forms.CharField(max_length=30, required=True)
 
 class AddCollaboratorForm(forms.Form):
     collaboratorUsername = forms.CharField(max_length=100, required=True)
+
+class IssueCreateForm(forms.ModelForm):
+    class Meta:
+        model=Issue
+        fields=['topic','description','tags']
--- Repos/urls.py
+++ Repos/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('initrepo/', views.init_Repo, name='initrepo'),
     path('fork/<id>/', views.fork, name='fork'),
+    path('<owner>/<name>/issues/', views.issue_list, name='issue_list'),
+    path('<owner>/<name>/issues/new/', views.create_issue, name='create_issue'),
     path('<owner>/<name>/', views.detail_repo, name='detail_repo'),
     path('delete/<owner>/<name>/', views.delete_repo, name='delete_repo'),
     path('star/', views.star, name='star'),
