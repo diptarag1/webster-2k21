6dee842a92a0621806e7b020ae89ec5522f26a10
siddheshpatil777 <siddheshpatil777@gmail.com>
2021-10-15 12:36:02 +0400

    profile page layout changed

--- home/views.py
+++ home/views.py
@@ -3,7 +3,9 @@ from Repos.models import Repo
 # Create your views here.
 
 def index(request):
-    context={}
-    repos=Repo.objects.filter(owner=request.user)
-    context['repos']=repos
-    return render(request,'home/index.html',context=context)
+    if request.user.is_authenticated:
+        context={}
+        repos=Repo.objects.filter(owner=request.user)
+        context['repos']=repos
+        return render(request,'home/index.html',context=context)
+    return render(request, 'user/signin.html')
