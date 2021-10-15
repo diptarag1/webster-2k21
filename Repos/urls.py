8c2bfb4b97bed0a90b27b6cf2323b4ef54e10a17
DuP-491 <divyansh.20198117@mnnit.ac.in>
2021-10-16 00:20:05 +0530

    dynamic url mapping

--- Repos/urls.py
+++ Repos/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,re_path
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -7,9 +7,9 @@ urlpatterns = [
     path('fork/<id>/', views.fork, name='fork'),
     path('<owner>/<name>/issues/', views.issue_list, name='issue_list'),
     path('<owner>/<name>/issues/new/', views.create_issue, name='create_issue'),
+    re_path(r'^(?P<owner>[^/]+)/(?P<name>[^/]+)/(?P<subpath>)/$', views.detail_repo, name='detail_repo1'),
     path('<owner>/<name>/', views.detail_repo, name='detail_repo'),
-    path('<owner>/<name>/', views.detail_repo, name='detail_repo1'),
-    path('<owner>/<name>/<subpath>', views.detail_repo, name='detail_repo2'),
+    path('<owner>/<name>/<path:subpath>', views.detail_repo, name='detail_repo2'),
     path('delete/<owner>/<name>/', views.delete_repo, name='delete_repo'),
     path('star/', views.star, name='star'),
     path('<ownerUsername>/<repoName>/addremovecollaborator', views.add_remove_collaborator,
