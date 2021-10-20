018155e1f5f3aa7c89debb9eb6dec63a92fbf86c
siddheshpatil777 <siddheshpatil777@gmail.com>
2021-10-20 14:15:29 +0400

    basic activity wala part

--- Repos/models.py
+++ Repos/models.py
@@ -6,24 +6,25 @@ from taggit.managers import TaggableManager
 
 from .serverLocation import rw_dir
 
+
 # Create your models here.
 class Repo(models.Model):
-    parent=models.ForeignKey("self",null=True,on_delete=models.CASCADE)
-    name=models.CharField(max_length=30)
+    parent = models.ForeignKey("self", null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
-    collaborators = models.ManyToManyField(User, related_name='collaborators',blank=True)
-    repoURL = models.CharField(max_length=30) #ownerName/repoName
-    star = models.ManyToManyField(User,related_name='star',blank=True)
+    collaborators = models.ManyToManyField(User, related_name='collaborators', blank=True)
+    repoURL = models.CharField(max_length=30)  # ownerName/repoName
+    star = models.ManyToManyField(User, related_name='star', blank=True)
     is_private = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.repoURL 
+        return self.repoURL
 
     def save(self, *args, **kwargs):
         gitRepo.init(os.path.join(rw_dir, self.repoURL))
-        self.repoURL=str(self.owner) + '/' + self.name
+        self.repoURL = str(self.owner) + '/' + self.name
         super().save(*args, **kwargs)
-    
+
     def delete(self, *args, **kwargs):
         shutil.rmtree(os.path.join(rw_dir, self.repoURL))
         super().delete(*args, **kwargs)
@@ -31,14 +32,50 @@ class Repo(models.Model):
     def create_fork(self, parent):
         parentGitRepo = gitRepo(os.path.join(rw_dir, parent.repoURL))
         curRepo = parentGitRepo.clone(os.path.join(rw_dir, self.repoURL))
-        curRepo.delete_remote('origin') #deleting origin remote which is automatically created
+        curRepo.delete_remote('origin')  # deleting origin remote which is automatically created
+
 
 class Issue(models.Model):
-    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='author')
-    repo = models.ForeignKey(Repo,on_delete=models.CASCADE,related_name='repot')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    repo = models.ForeignKey(Repo, on_delete=models.CASCADE, related_name='repot')
     topic = models.CharField(max_length=120)
     description = models.TextField()
     tags = TaggableManager()
 
     def __str__(self):
         return self.topic
+
+
+class Activity(models.Model):
+    thisGuy = models.ForeignKey(User, null=False, on_delete=models.CASCADE, related_name='thisGuy')
+    activity_type = models.SmallIntegerField()
+    thatGuy = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='thatGuy')
+    thatRepo = models.ForeignKey(Repo, null=True, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+
+    @staticmethod
+    def createdRepo(thisGuy, thatRepo):
+        return Activity(thisGuy=thisGuy, activity_type=1, thatGuy=None, thatRepo=thatRepo)
+
+    @staticmethod
+    def forkedRepo(thisGuy, thatGuy, thatRepo):
+        return Activity(thisGuy=thisGuy, activity_type=2, thatGuy=thatGuy, thatRepo=thatRepo)
+
+    @staticmethod
+    def starredRepo(thisGuy, thatRepo):
+        return Activity(thisGuy=thisGuy, activity_type=3, thatGuy=thatRepo.owner, thatRepo=thatRepo)
+
+    @staticmethod
+    def startedFollowing(thisGuy, thatGuy):
+        return Activity(thisGuy=thisGuy, activity_type=4, thatGuy=thatGuy, thatRepo=None)
+
+    def __str__(self):
+        if self.activity_type == 1:
+            return self.thisGuy.username + " created repository " + self.thatRepo.name + " on "
+        if self.activity_type == 2:
+            return self.thisGuy.username + " forked repository " + self.thatGuy.username + "/" + self.thatRepo.name + " on "
+        if self.activity_type == 3:
+            return self.thisGuy.username + " started following  " + self.thatGuy.username + " on "
+        if self.activity_type == 4:
+            return self.thisGuy.username + " starred " + self.thatGuy.username + "/" + self.thatRepo.name + " on "
+        return "some error"
--- home/views.py
+++ home/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from Repos.models import Repo
+from Repos.models import Repo, Activity
+
+
 # Create your views here.
 
 def index(request):
     if request.user.is_authenticated:
         context={}
         repos=Repo.objects.filter(owner=request.user)
+        activities=Activity.objects.all()
         context['repos']=repos
+        context['activities'] = activities
         return render(request,'home/index.html',context=context)
     return render(request, 'user/signin.html')
