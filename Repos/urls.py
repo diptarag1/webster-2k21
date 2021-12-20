5f73beda58798ea0e69a518ee94e27a14686654b
Diptarag <diptarag1@gmail.com>
2021-12-20 17:58:55 +0530

    Fixed braching

--- Repos/urls.py
+++ Repos/urls.py
@@ -12,8 +12,9 @@ urlpatterns = [
     path('<owner>/<name>/issues/new/', views.create_issue, name='create_issue'),
     path('<ownerUsername>/<repoName>/addremovecollaborator/', views.add_remove_collaborator,
          name='add-remove-collaborator'),
-    path('<owner>/<name>/<branch>', views.detail_repo, name='detail_repo'),
+
     path('<owner>/<name>/', views.detail_repo, name='detail_repo'),
-    re_path(r'^(?P<owner>[^/]+)/(?P<name>[^/]+)/(?P<subpath>)/$', views.detail_repo, name='detail_repo1'),
-    path('<owner>/<name>/<path:subpath>/', views.detail_repo, name='detail_repo2'),
+    path('<owner>/<name>/<branch>/', views.detail_repo, name='detail_repo1'),
+    path('<owner>/<name>/<branch>/<path:subpath>/', views.detail_repo, name='detail_repo2'),
+    #re_path(r'^(?P<owner>[^/]+)/(?P<name>[^/]+)/(?P<subpath>)/$', views.detail_repo, name='detail_repo1'),
 ]
